afff631731efd21ea4fa7a893ec26efaeaf782e3
ytoyoshi <113228101+ytoyoshi@users.noreply.github.com>
2023-07-25 21:40:43 +0900

    20230725

--- python/app/ToDoApp/todo/urls.py
+++ python/app/ToDoApp/todo/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from . import views
 from .views import TodoList, CreateList, DetailList
 
 urlpatterns = [
     path("list/", TodoList.as_view(),name="list"),
     path("create/", CreateList.as_view(),name="create"),
-    path("detail/<int:id>", DetailList.as_view(),name="detail"),
+    path("detail/<int:pk>", DetailList.as_view(),name="detail"),
     path("complete/<int:id>", views.Complete,name="complete"),
 ]
--- python/app/ToDoApp/todo/views.py
+++ python/app/ToDoApp/todo/views.py
@@ -1,12 +1,13 @@
-from typing import Any
+from typing import Any, Dict
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView ,View
+from django.views.generic import ListView, UpdateView, CreateView
+from django.views import generic
 from .models import TodoModel
 from django.views.decorators.http import require_POST
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
-from . import models
+from . import models,forms
 from .forms import TodoForm
 
 # Create your views here.
@@ -15,9 +16,11 @@ class TodoList(ListView):
     template_name = 'TodoList.html'
     queryset = models.TodoModel.objects.filter(complete=TodoModel.CompleteType.INCOMPLETE)
 
-class DetailList(DetailView):
+class DetailList(UpdateView):
     template_name = 'detail.html'
     model = TodoModel
+    form_class = TodoForm
+    success_url = reverse_lazy('list')
 
 class CreateList(CreateView):
     template_name = 'create.html'
